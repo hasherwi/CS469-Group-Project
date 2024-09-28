af9a78169769b7cb63bc385ddc985b32705d0f93
Harrison Sherwin <70494788+hasherwi@users.noreply.github.com>
2024-09-28 16:01:04 -0600

    Update client.c
    Update @file and @date.

--- client.c
+++ client.c
@@ -1,9 +1,9 @@
 // So far this is just a copy of my code from Assignment 4.
 
 /**
-* @file ssl-client.c
+* @file client.c
 * @author Harrison Sherwin
-* @date  22 September 2024
+* @date  28 September 2024
  * @brief This program is a small client application that establishes a secure
           TCP connection to a server and simply exchanges messages. It uses a
           SSL/TLS connection using X509 certificates generated with the openssl
